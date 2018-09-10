4c520fb8f1c5f334fb827df002bfedab4ad91e24
Prakash Joshi <mejoshi8@gmail.com>
2018-09-09 23:32:46 -0500

    Finished Writing Draw Namespace

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate piston_window;
 
 // Another namespace
 mod draw;
+mod snake;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/snake.rs
@@ -0,0 +1,9 @@
+use std::collections::LinkedList;
+use piston_window::{Context, G2d};
+use piston_window::types::Color;
+
+
+use draw:: draw_block;
+
+
+const SNAKE_COLOR: Color = [0.00]
